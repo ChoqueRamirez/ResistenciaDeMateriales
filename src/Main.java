e48c9bf56160ff56f80e837860bc316f0552ccf5
Willi <felix.chrz@gmail.com>
2022-01-29 23:52:10 -0300

    Agrego mas detalles a los baricentros

--- src/Main.java
+++ src/Main.java
@@ -14,24 +14,24 @@ public class Main {
 
 //
 //
-//        System.out.println("");
-//
-//        BarraCircular Viga2 = new BarraCircular(5, 500, 210, 80);
-//        Viga2.area();
-//        Viga2.Baricentro();
-//        Viga2.MomentosPolarDeInercia();
-//        Viga2.RigidezAxial();
-//        Viga2.CondicionRigidezFlexional();
-//        Viga2.CondicionRigidezTorsional();
-//
-//        System.out.println("");
+        System.out.println("");
 
-//        IPNDobleT Viga3 = new IPNDobleT(22, 9.8F, 500, 210, 85, "220");
-//        Viga3.Baricentro();
-//        Viga3.area();
-//        Viga3.MomentosDeInercia();
-//        Viga3.RigidezAxial();
-//        Viga3.CondicionRigidezFlexional();
+        BarraCircular Viga2 = new BarraCircular(5, 500, 210, 80);
+        Viga2.area();
+        Viga2.Baricentro();
+        Viga2.MomentosPolarDeInercia();
+        Viga2.RigidezAxial();
+        Viga2.CondicionRigidezFlexional();
+        Viga2.CondicionRigidezTorsional();
+
+        System.out.println("");
+
+        IPNDobleT Viga3 = new IPNDobleT(22, 9.8F, 500, 210, 85, "220");
+        Viga3.Baricentro();
+        Viga3.area();
+        Viga3.MomentosDeInercia();
+        Viga3.RigidezAxial();
+        Viga3.CondicionRigidezFlexional();
 
 
 
